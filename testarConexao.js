1bc916a891547444e54fb7f781e593f814991811
henrytavares-sketch <henry_tavares@estudante.sesisenai.org.br>
2026-09-10 16:35:14 -0300

    a

--- testarConexao.js
+++ testarConexao.js
@@ -1,7 +1,8 @@
-import pool from './db.js';
+import pool from './db.js'
 
 async function testarConexao() {
     try {
+        console.log('DB_NAME lido:', process.env.DB_NAME);
         const resultado = await pool.query('SELECT * FROM equipamentos');
         console.log(`Conexão OK. ${resultado.rows.length} equipamento(s) encontrado(s):`);
         console.table(resultado.rows);
