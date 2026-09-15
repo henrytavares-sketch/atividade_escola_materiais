50ddbaa7250e59631dfe16459925cb9586602796
henrytavares-sketch <henry_tavares@estudante.sesisenai.org.br>
2026-09-15 15:01:48 -0300

    Entrega professor

--- sistema.emprestimo/src/routes/equipamento.routes.js
+++ sistema.emprestimo/src/routes/equipamento.routes.js
@@ -57,5 +57,3 @@ EquipamentoRouter.patch('/:id/disponibilidade', async (req, res) => {
 
 export default EquipamentoRouter;
 
-EquipamentoRouter.get('/status', (req, res) =>
- { return res.json({ status: 'Servidor ativo e rodando!' }); });
--- sistema.emprestimo/src/server.js
+++ sistema.emprestimo/src/server.js
@@ -5,6 +5,9 @@ const app = express();
 app.use(express.json());
 app.use('/equipamentos', EquipamentoRouter);
 
+app.get('/status', (req, res) =>
+ { return res.json({ status: 'Servidor ativo e rodando!' }); });
+
 app.listen(3000, () => {
   console.log('Servidor rodando na porta http://localhost:3000');
 });
